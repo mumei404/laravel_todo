5502505acd1fd075733264fb9f6acbedaf48ea72
shimazaki <lozld30@gmail.com>
2019-07-08 21:57:49 +0900

    create task routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::get('/', function(){
+	return view('tasks');
 });
+
+Route::post('/task', function(Request $request){
+	//
+});
+
+Route::delete('/task/{task}', function(Task $task){
+	//
+});
+
