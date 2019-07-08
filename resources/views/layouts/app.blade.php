1b455124acdad429988f249a6c34d65789298aa6
shimazaki <lozld30@gmail.com>
2019-07-08 22:08:54 +0900

    create layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Laravel Quickstart - Basic</title>
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+	<link hred= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"rel="stylesheet">
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="conteiner">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="{{ url('/') }}">
+					Task List
+				</a>
+			</div>
+		</div>
+	</nav>
+
+	@yield('content')
+
+	<script src=>"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"</script>
+</body>
+</html>
